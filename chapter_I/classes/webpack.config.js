ded7ad2e78cf20856456544b40587464064863fd
debora-fortunato <debora.fortunato@hp.com>
2022-06-25 10:18:50 -0300

    Add typescript

--- chapter_I/classes/webpack.config.js
+++ chapter_I/classes/webpack.config.js
@@ -5,13 +5,13 @@ const ReactRefreshWebplackPlugin = require('@pmmmwh/react-refresh-webpack-plugin
 const isDevEnv = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     resolve: {
-        extensions: ['.js', '.jsx'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
     },
     plugins: [
         isDevEnv && new ReactRefreshWebplackPlugin(),
@@ -26,7 +26,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.jsx$/,
+                test: /\.(j|t)sx$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
