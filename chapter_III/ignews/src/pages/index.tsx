88c935f7af3930b65a824a91d5e3094e708b1218
debora-fortunato <debora.fortunato@hp.com>
2022-06-30 21:05:08 -0300

    Ignews integration with Strip API

---
+++ chapter_III/ignews/src/pages/index.tsx
@@ -0,0 +1,50 @@
+import { GetServerSideProps } from 'next';
+import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe';
+import styles from './home.module.scss'
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
+
+  return (
+    <>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span className={styles.subText}>👏 Hey, welcome</span>
+          <h1> News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for {product.amount} month.</span>
+          </p>
+          <SubscribeButton priceId={product.priceId}/>
+        </section>
+        <img src="/images/avatar.svg" alt= "Logo"/>
+        
+      </main>
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1LGX85LHJV2NrJDquXFj9QA7')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product
+    }
+  }
+}
